d01ab3114517bf12f51405dae0969a8685959a2a
Ammar <s95ammar@gmail.com>
2020-06-05 23:01:55 +0300

    Added SettingsPresenterTest

---
+++ app/src/test/java/com/s95ammar/mvpcurrencyconverter/ui/settings/SettingsPresenterTest.kt
@@ -0,0 +1,57 @@
+package com.s95ammar.mvpcurrencyconverter.ui.settings
+
+import com.s95ammar.mvpcurrencyconverter.RxJavaTestingUtil
+import com.s95ammar.mvpcurrencyconverter.model.FakeRepository
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.BeforeClass
+
+class SettingsPresenterTest {
+
+    companion object {
+        @BeforeClass
+        @JvmStatic
+        fun setUpRxSchedulers() = RxJavaTestingUtil.setUpRxSchedulers()
+    }
+
+    private lateinit var settingsPresenter: SettingsPresenter
+
+    @Before
+    fun setupPresenter() {
+        val fakeRepository = FakeRepository()
+        val homeCountryCode = "UA"
+        val baseCurrencyCode = "USD"
+        val homeCurrencyCode: String? = null
+
+        settingsPresenter = SettingsPresenter(
+            fakeRepository,
+            homeCountryCode,
+            baseCurrencyCode,
+            homeCurrencyCode
+        )
+    }
+
+    @Test
+    fun loadCurrenciesList_homeCurrencyCodeIsNull_setsHomeCurrencyCodeAndLoadsCurrenciesList() {
+        settingsPresenter.currencies = null
+        settingsPresenter.homeCurrencyCode = null
+
+        settingsPresenter.loadCurrenciesList()
+
+        assertNotNull(settingsPresenter.homeCurrencyCode)
+        assertNotNull(settingsPresenter.currencies)
+    }
+
+    @Test
+    fun loadCurrenciesList_homeCurrencyCodeIsNotNull_loadsCurrenciesList() {
+        settingsPresenter.currencies = null
+        settingsPresenter.homeCurrencyCode = "UAH"
+
+        settingsPresenter.loadCurrenciesList()
+
+        assertNotNull(settingsPresenter.currencies)
+    }
+
+}
